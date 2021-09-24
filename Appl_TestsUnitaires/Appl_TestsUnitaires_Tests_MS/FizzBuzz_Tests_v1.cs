d927b731aecf33df57882fe5a68c677a0a5bc419
valerieturgeon <valerie.turgeon.cem@gmail.com>
2021-09-23 20:03:07 -0400

    INIT Création projet initial
    Révision notions de base des tests unitaires

---
+++ Appl_TestsUnitaires/Appl_TestsUnitaires_Tests_MS/FizzBuzz_Tests_v1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appl_TestsUnitaires_Tests_MS
+{
+  [TestClass]
+  public class FizzBuzz_Tests_v1
+  {
+    // Arrange
+
+    // Act
+    var result = FizzBuzz.GetOutput(response);
+
+    // Assert
+
+    Assert.That(result, Is.EqualTo(expectedResult));
+  }
+}
